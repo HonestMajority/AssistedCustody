d86cfc11120ae421f51ccc4cf91c91c28783652e
Tim <enlightened.dev21@gmail.com>
2025-09-11 21:05:24 +0200

    Configure RPC client via appsettings and DI

--- Pages/BitcoinInfo.cshtml.cs
+++ Pages/BitcoinInfo.cshtml.cs
@@ -1,23 +1,21 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using NBitcoin;
 using NBitcoin.RPC;
-using System.Net;
 
 namespace AssistedCustody.Pages;
 
 public class BitcoinInfoModel : PageModel
 {
     public string? InfoJson { get; private set; }
+    private readonly RPCClient _client;
+
+    public BitcoinInfoModel(RPCClient client)
+    {
+        _client = client;
+    }
 
     public async Task OnGet()
     {
-        var rpcUser = Environment.GetEnvironmentVariable("BITCOIN_RPC_USER") ?? "bitcoin";
-        var rpcPassword = Environment.GetEnvironmentVariable("BITCOIN_RPC_PASSWORD") ?? "bitcoin";
-        var rpcUrl = Environment.GetEnvironmentVariable("BITCOIN_RPC_URL") ?? "http://localhost:18443";
-
-        var credentials = new NetworkCredential(rpcUser, rpcPassword);
-        var client = new RPCClient(credentials, new Uri(rpcUrl), Network.RegTest);
-        var response = await client.SendCommandAsync(RPCOperations.getblockchaininfo);
+        var response = await _client.SendCommandAsync(RPCOperations.getblockchaininfo);
         InfoJson = response.ResultString;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
+using NBitcoin;
+using NBitcoin.RPC;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton(sp =>
+{
+    var config = sp.GetRequiredService<IConfiguration>().GetSection("BitcoinRpc");
+    var credentials = new NetworkCredential(config["Username"], config["Password"]);
+    var url = new Uri(config["Url"]!);
+    return new RPCClient(credentials, url, Network.RegTest);
+});
 
 var app = builder.Build();
 

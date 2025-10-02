5287d4f12a34ecb4245c40f2255583298b7ded18
nordbjorn <enlightened.dev21@gmail.com>
2025-10-02 19:54:24 +0200

    WIP

--- Program.cs
+++ Program.cs
@@ -33,6 +33,12 @@ builder.Services.AddSingleton(sp =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AssistedCustodyDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
